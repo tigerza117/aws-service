fd11ee14e3f3e064e2089a234d6d14f0def54b8d
tigerza117 <noppawanza1171@gmail.com>
2022-12-16 14:55:11 +0700

    add dependencies and init routes.

---
+++ app/src/lib/Login.svelte
@@ -0,0 +1,22 @@
+<script>
+    import axios from "axios";
+    let name = "";
+    let email = "world";
+    let password = "";
+    const data = { name: name, email: email, password: password };
+    function handleClick() {
+        axios.post("https://i-here-ji.tigerza117.xyz/register", data).then(result => {
+            if (result.message === "OK") {
+                alert("Yes");
+            } else {
+                alert("No");
+            }
+        });
+    }
+</script>
+
+<input bind:value={name} type="name" name="name"/>
+<input bind:value={email} type="email" name="email"/>
+<input bind:value={password} type="password" name="password"/>
+<button on:click={handleClick}>Submit</button>
+<h1>Log in</h1>
---
+++ app/src/routes/Home.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-3xl font-bold underline bg-zinc-300">
+    Hello world!
+</h1>
