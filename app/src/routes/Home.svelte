f960f21449c9efe9c718878ddff3be81fe8e6cc1
larkingz01 <63070017@kmitl.ac.th>
2022-12-18 20:20:51 +0700

    update home and add not_found page

--- app/src/routes/Home.svelte
+++ app/src/routes/Home.svelte
@@ -1,3 +1,15 @@
-<h1 class="text-3xl font-bold underline bg-zinc-300">
-    Hello world!
-</h1>
+<script>
+    import {link} from 'svelte-spa-router'
+</script>
+
+<style>
+    body{
+        text-align: center;
+        justify-content: center;
+    }
+</style>
+<body>
+    <h1> Hello to 3x2 Banking</h1>
+    <p>Have an accout? <a href="/login" use:link>Login</a></p>
+    <p>Become our member! <a href="/Register" use:link>Register</a></p>
+</body>
---
+++ app/src/routes/NotFound.svelte
@@ -0,0 +1,7 @@
+<style>
+    h1{
+        text-align: center;
+        justify-content: center;
+    }
+</style>
+<h1>Not found</h1>
