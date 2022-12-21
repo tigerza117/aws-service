cb4418b861780e2e775bb7a5f0c4f6391df29f18
tigerza117 <noppawanza1171@gmail.com>
2022-12-21 23:19:54 +0700

    dot end load and http for health check.

--- api/cmd/transaction-consumer/main.go
+++ api/cmd/transaction-consumer/main.go
@@ -1,22 +1,30 @@
 package main
 
 import (
-	"api/model"
-	"api/query"
 	"context"
 	"flag"
 	"fmt"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/sqs"
-	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"log"
 	"os"
 	"strconv"
 	"time"
+
+	"api/model"
+	"api/query"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/aws/aws-sdk-go-v2/service/sqs/types"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	queue := flag.String("q", "", "The name of the queue")
 	flag.Parse()
 
@@ -54,56 +62,64 @@ func main() {
 
 	queueURL := result.QueueUrl
 
-	for {
-		time.Sleep(time.Second)
-		gMInput := &sqs.ReceiveMessageInput{
-			MessageAttributeNames: []string{
-				string(types.QueueAttributeNameAll),
-			},
-			QueueUrl:            queueURL,
-			MaxNumberOfMessages: 1,
-			VisibilityTimeout:   int32(100),
-		}
-
-		msgResult, err := GetMessages(context.TODO(), client, gMInput)
-		if err != nil {
-			fmt.Println("Got an error receiving messages:")
-			fmt.Println(err)
-			return
-		}
-
-		if msgResult.Messages != nil {
-			fmt.Println("Message ID:     " + *msgResult.Messages[0].MessageId)
-			fmt.Println("Message Handle: " + *msgResult.Messages[0].ReceiptHandle)
-			idStr := msgResult.Messages[0].MessageAttributes["TxID"].StringValue
-			id, err := strconv.Atoi(*idStr)
-			if err != nil {
-				panic(err)
-			}
-			t, err := query.Tx.Where(query.Tx.ID.Eq(uint(id))).First()
-			if err != nil {
-				panic(err)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			gMInput := &sqs.ReceiveMessageInput{
+				MessageAttributeNames: []string{
+					string(types.QueueAttributeNameAll),
+				},
+				QueueUrl:            queueURL,
+				MaxNumberOfMessages: 1,
+				VisibilityTimeout:   int32(100),
 			}
 
-			if err := query.Q.Transaction(func(tx *query.Query) error {
-				if _, err := tx.WithContext(context.Background()).Account.Where(tx.Account.ID.Eq(t.DstAccountID)).UpdateSimple(tx.Account.Balance.Add(t.Amount)); err != nil {
-					return err
+			msgResult, err := GetMessages(context.TODO(), client, gMInput)
+			if err != nil {
+				fmt.Println("Got an error receiving messages:")
+				fmt.Println(err)
+				return
+			}
+
+			if msgResult.Messages != nil {
+				fmt.Println("Message ID:     " + *msgResult.Messages[0].MessageId)
+				fmt.Println("Message Handle: " + *msgResult.Messages[0].ReceiptHandle)
+				idStr := msgResult.Messages[0].MessageAttributes["TxID"].StringValue
+				id, err := strconv.Atoi(*idStr)
+				if err != nil {
+					panic(err)
 				}
-				if t.AccountID != nil {
-					if _, err := tx.WithContext(context.Background()).Account.Where(tx.Account.ID.Eq(*t.AccountID), tx.Account.Balance.Gte(t.Amount)).UpdateSimple(tx.Account.Balance.Sub(t.Amount)); err != nil {
+				t, err := query.Tx.Where(query.Tx.ID.Eq(uint(id))).First()
+				if err != nil {
+					panic(err)
+				}
+
+				if err := query.Q.Transaction(func(tx *query.Query) error {
+					if _, err := tx.WithContext(context.Background()).Account.Where(tx.Account.ID.Eq(t.DstAccountID)).UpdateSimple(tx.Account.Balance.Add(t.Amount)); err != nil {
 						return err
 					}
+					if t.AccountID != nil {
+						if _, err := tx.WithContext(context.Background()).Account.Where(tx.Account.ID.Eq(*t.AccountID), tx.Account.Balance.Gte(t.Amount)).UpdateSimple(tx.Account.Balance.Sub(t.Amount)); err != nil {
+							return err
+						}
+					}
+					t.Status = model.TransactionSuccess
+					if _, err := tx.WithContext(context.Background()).Tx.Updates(t); err != nil {
+						return err
+					}
+					return nil
+				}); err != nil {
+					panic(err)
 				}
-				t.Status = model.TransactionSuccess
-				if _, err := tx.WithContext(context.Background()).Tx.Updates(t); err != nil {
-					return err
-				}
-				return nil
-			}); err != nil {
-				panic(err)
+			} else {
+				fmt.Println("No messages found")
 			}
-		} else {
-			fmt.Println("No messages found")
 		}
-	}
+	}()
+
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Am healthy fuck you")
+	})
+	app.Listen(os.Getenv("LISTEN"))
 }
